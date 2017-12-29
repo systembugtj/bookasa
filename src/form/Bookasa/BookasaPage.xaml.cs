273f673290f69d3ceded8b2521c7a8a9def6fe73
systembug <albert_lee@hotmail.com>
2017-12-29 12:23:26 -0800

    merge

---
+++ src/form/Bookasa/BookasaPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Bookasa
+{
+	public partial class BookasaPage : ContentPage
+	{
+		public BookasaPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ src/native/Bookasa/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Bookasa
+{
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+	}
+}
